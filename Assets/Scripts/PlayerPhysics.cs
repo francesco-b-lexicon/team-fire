be58baabe4d3b49c71ef2a897e5829de35a0122c
Francesco Battaglini <francesco.battaglini@lexicondigital.com.au>
2022-09-17 10:51:17 +1000

    feat: add some bounciness to birb

---
+++ Assets/Scripts/PlayerPhysics.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPhysics : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Vector2 LastVelocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LastVelocity = rb.velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.collider.name == "Floor")
+        {
+            var speed = LastVelocity.magnitude * .4f;
+            var direction = Vector2.Reflect(LastVelocity.normalized, col.contacts[0].normal);
+
+            rb.velocity = direction * Mathf.Max(speed, 0f);
+        }
+    }
+}
