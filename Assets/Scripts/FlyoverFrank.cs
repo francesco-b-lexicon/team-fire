8a2db5e3deaa55e7935f147533fa40a37efea186
Ben A <ben.atkinson@lexicondigital.com.au>
2022-09-18 12:01:55 +1000

    delete frank when he's off screen

--- Assets/Scripts/FlyoverFrank.cs
+++ Assets/Scripts/FlyoverFrank.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class FlyoverFrank : MonoBehaviour
 {
+
     public float speed = 1f;
+    private float endPos = 0;
+
     // Update is called once per frame
+    void Start()
+    {
+        endPos = transform.position.x + 100;
+    }
     void Update()
     {
         transform.Translate(Vector3.right * Time.deltaTime * speed);
+        if (transform.position.x > endPos)
+        {
+            Destroy(gameObject);
+        }
     }
 }
