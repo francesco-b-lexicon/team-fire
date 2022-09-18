d529636c1c7c37254f9097e5904000e8221ae951
Francesco Battaglini <francesco.battaglini@lexicondigital.com.au>
2022-09-18 14:57:22 +1000

    add rock destroyable

---
+++ Assets/Scripts/Hittables/Rock.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour, IHittable
+{
+    public AudioClip hitSound;
+    public AudioClip deathSound;
+    public Sprite spriteHurt;
+    public Sprite spriteDestroyed;
+    public bool CanBeHit { get; set; }
+    public int health = 3;
+    public float immuneWindow = 0.5f;
+    private float immuneTimer = 0;
+    private bool dead = false;
+    private AudioSource sfx;
+    private SpriteRenderer sprite;
+    private Color normalColor;
+    public void Hit()
+    {
+        if (immuneTimer <= 0)
+        {
+            health -= 1;
+            Debug.Log("ow! I only have " + health + " health remaining");
+            if (health <= 0)
+            {
+                sfx.PlayOneShot(deathSound);
+                GetComponent<Collider2D>().enabled = false;
+                // GetComponent<ParticleSystem>().Emit();
+                dead = true;
+                sprite.sprite = spriteDestroyed;
+
+            }
+            else
+            {
+                sprite.sprite = spriteHurt;
+                sfx.PlayOneShot(hitSound);
+            }
+            CanBeHit = false;
+            immuneTimer = immuneWindow;
+        }
+    }
+
+    void Awake()
+    {
+        sfx = GetComponent<AudioSource>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+    void Update()
+    {
+        if (dead && !sfx.isPlaying)
+        {
+            Destroy(gameObject);
+        }
+        if (immuneTimer > 0)
+        {
+            immuneTimer -= Time.deltaTime;
+        }
+        else if (!dead)
+        {
+            CanBeHit = true;
+        }
+
+    }
+}
